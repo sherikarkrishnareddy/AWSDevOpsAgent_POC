13173dbbfa95dab11ac898b4e9cf0979ed386020
sherikarkrishnareddy <54585309+sherikarkrishnareddy@users.noreply.github.com>
2025-12-16 14:44:14 +0530

    Add environment configuration to CDK stack

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 from devops_agent_poc.devops_agent_poc_stack import DevOpsAgentPOCStack
-
+env = cdk.Environment(
+    account="453971898162",
+    region="us-east-1"
+)
 app = cdk.App()
-stack = DevOpsAgentPOCStack(app, "DevOpsAgentPOC")
-cdk.CfnOutput(app, "ApiUrl", value=stack.api_url)
+stack = DevOpsAgentPOCStack(app, "DevOpsAgentPOC",env=env)
+
 
 app.synth()
